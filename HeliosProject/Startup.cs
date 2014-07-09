3ac9af3e044d54301736d822b0744c4be80da824
Jonathan Channon <jonathan@Jonathans-MacBook-Pro.local>
2014-07-09 19:01:48 +0100

    initial

---
+++ HeliosProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace HeliosProject
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context => context.Response.WriteAsync("YO"));
+        }
+    }
+}
---
+++ SystemWebProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace HeliosBug
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context => context.Response.WriteAsync("YO"));
+        }
+    }
+}
